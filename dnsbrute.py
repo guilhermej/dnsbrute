6b8ca53caf93346873cd01e387211373064c065c
Guilherme Junqueira <guilhermej@users.noreply.github.com>
2017-06-01 05:39:12 -0300

    Add files via upload

---
+++ dnsbrute.py
@@ -0,0 +1,28 @@
+import sys
+import dns.resolver
+
+argumentos = sys.argv	# le os argumentos do comando
+try:
+    dominio = argumentos[1]
+    lista = argumentos[2]
+except:
+    print("Faltam argumentos no comando")
+    sys.exit(1)
+
+# abre a wordlist
+try:
+    arquivo = open(lista)
+    linhas = arquivo.read().splitlines()
+except:
+    print("Arquivo nao encontrao ou invalido")
+    sys.exit(1)
+
+# para cada linha da wordlist testa o dns
+for linha in linhas:
+    subdominio = linha + '.bancocn.com'
+    try:
+        respostas = dns.resolver.query(subdominio, 'a')
+        for resultado in respostas:
+            print subdominio, resultado
+    except:
+        pass
